dc6546fe64efd8f2e56876a91c80f6be911017c0
Bob Lehman <blehman12@comcast.net>
2026-02-19 12:18:06 -0800

    Fix FixAdminUserRole migration - use integer value not symbol key

--- db/migrate/20260219192419_fix_admin_user_role.rb
+++ db/migrate/20260219192419_fix_admin_user_role.rb
@@ -1,7 +1,8 @@
 class FixAdminUserRole < ActiveRecord::Migration[7.1]
   def up
-    # Fix any user named 'Admin User' that got created with the wrong role
-    User.where(first_name: 'Admin', last_name: 'User').update_all(role: User.roles[:admin])
+    # User.roles has string keys, not symbol keys
+    # User.roles['admin'] returns 1, User.roles[:admin] returns nil (bug!)
+    User.where(first_name: 'Admin', last_name: 'User').update_all(role: 1)
   end
 
   def down
